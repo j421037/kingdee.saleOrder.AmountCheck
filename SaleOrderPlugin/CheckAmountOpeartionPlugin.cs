08ff2e4067811b95a942a4db50ee2cb7fa651b22
j421037 <421037083@qq.com>
2019-12-23 18:00:48 +0800

    1223

--- SaleOrderPlugin/CheckAmountOpeartionPlugin.cs
+++ SaleOrderPlugin/CheckAmountOpeartionPlugin.cs
@@ -15,6 +15,7 @@ namespace YHT.K3.SCM.App.Sal.ServicePlugIn
 {
     /// <summary>
     /// 销售订单金额设计插件
+    /// 客户应收 = 销售应收 - 收款 + 收款退款 - 应收核销    //暂定
     /// </summary>
     /// 
     //[System.ComponentModel.Description("销售订单金额审计插件")]
@@ -47,7 +48,7 @@ namespace YHT.K3.SCM.App.Sal.ServicePlugIn
             }
 
             e.Cancel = true;
-            e.CancelMessage = string.Format("客户id：{0}, 整单金额: {1}, 客户额度：{2}", cust_id, allAmount, LoaCredit(cust_id));
+            e.CancelMessage = string.Format("客户id：{0}, 整单金额: {1}, 客户额度：{2}, 客户全部应收： {3}", cust_id, allAmount, LoaCredit(cust_id), $"{LoadCustAR(cust_id):N}");
         }
 
         /// <summary>
@@ -74,6 +75,60 @@ namespace YHT.K3.SCM.App.Sal.ServicePlugIn
             }
 
             return credit > 0 ? credit : DEFAULT_CREDIT ;
+        }
+
+        /// <summary>
+        /// 读取客户应收款汇总
+        /// </summary>
+        /// <param name="cust_id"></param>
+        /// <returns></returns>
+        private decimal LoadCustAR(string cust_id)
+        {
+            decimal total = 0;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("SELECT sum(FallAmountFor) as amount from t_AR_receivable WHERE fcustomerID = {0} and Fdocumentstatus = 'C' ", cust_id);
+
+            using (IDataReader reader = DBUtils.ExecuteReader(this.Context, sb.ToString()))
+            {
+                if (reader.Read())
+                {
+                    string amount = reader["amount"].ToString();
+
+                    if (!string.IsNullOrEmpty(amount))
+                    {
+                        total = Convert.ToDecimal(amount);
+                    }
+                }
+            }
+            return total;
+        }
+
+
+        /// <summary>
+        /// 读取客户收款
+        /// </summary>
+        /// <param name="cust_id"></param>
+        /// <returns></returns>
+        private decimal LoadCustReceivables(string cust_id)
+        {
+            decimal total = 0;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("SELECT SUM(FRECEIVEAMOUNT) AS amount FROM T_AR_RECEIVEBILL fcontactunit = {0} and Fdocumentstatus = 'C'", cust_id);
+
+            using (IDataReader reader = DBUtils.ExecuteReader(this.Context, sb.ToString()))
+            {
+                if (reader.Read())
+                {
+                    string amount = reader["amount"].ToString();
+
+                    if (!string.IsNullOrEmpty(amount))
+                    {
+                        total = Convert.ToDecimal(amount);
+                    }
+                }
+            }
+
+            return total;
         }
     }
 }
